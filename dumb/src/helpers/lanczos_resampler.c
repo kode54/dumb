99111038210a07af485be94a6e85dc43faa10eff
Carlos Rafael Giani <dv@pseudoterminal.org>
2013-05-22 17:37:36 +0200

    M_PI may not be defined
    When building with gcc 4.7.2 and -std=c99 , M_PI is not defined

--- dumb/src/helpers/lanczos_resampler.c
+++ dumb/src/helpers/lanczos_resampler.c
@@ -3,6 +3,10 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 #include "internal/lanczos_resampler.h"
 
 enum { LANCZOS_RESOLUTION = 8192 };
