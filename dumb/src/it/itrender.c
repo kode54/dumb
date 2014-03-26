cb8ab8c6f6ddef8a0938f771e3fad9d0ca6b2735
Chris Moeller <kode54@gmail.com>
2014-03-26 02:12:33 -0700

    Derp

--- dumb/src/it/itrender.c
+++ dumb/src/it/itrender.c
@@ -3946,11 +3946,11 @@ static void playing_volume_setup(DUMB_IT_SIGRENDERER * sigrenderer, IT_PLAYING *
 	playing->float_volume[1] *= vol;
 
     if (ramp_style == 0 || (ramp_style < 2 && playing->declick_stage == 2)) {
-		if (ramp_style < 2 || playing->declick_stage < 2) {
+		if (playing->declick_stage <= 2) {
 			playing->ramp_volume[0] = playing->float_volume[0];
 			playing->ramp_volume[1] = playing->float_volume[1];
 			playing->declick_stage = 2;
-		} else if (playing->declick_stage > 2) {
+		} else {
 			playing->float_volume[0] = 0;
 			playing->float_volume[1] = 0;
 			playing->ramp_volume[0] = 0;
